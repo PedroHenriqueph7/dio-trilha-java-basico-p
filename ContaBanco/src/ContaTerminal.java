054de1710bfc0b0b9c2c91dac515dbe242cb2e9b
PedroHenriqueph7 <pedrohenrizzx7@gmail.com>
2024-05-30 11:57:28 -0300

    meu projeto conta banco

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -20,6 +20,6 @@ public class ContaTerminal {
         double saldo = scanner.nextDouble();
 
         System.out.println("Olá "+nomeCliente+", obrigado por criar uma conta em nosso banco, sua agência é "+agencia+", conta "+numero+" e seu saldo "+saldo+" já está disponível para saque");
-
+        scanner.close();
     }
 }
